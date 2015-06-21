8b2adc55a11e7bf8b713a311966c05da598afc5d
tetsu <soutetsu@gmail.com>
2015-06-21 18:04:54 +0900

    update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    redirect_to [current_user.directories.first.root] if user_signed_in?
   end
 end
