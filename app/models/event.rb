b1a3bf65f372193c2c40fa6d38046ba7db0acfa3
tetsu <soutetsu@gmail.com>
2015-06-21 16:59:10 +0900

    update event validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :user
   belongs_to :directory
   belongs_to :fileitem
+
+  validates :user_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :directory_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :fileitem_id, numericality: { only_integer: true, greater_than: 0 }
 end
