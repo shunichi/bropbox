65f62dcb6b16ee70b17872e9b1ea096e7bec2969
tetsu <soutetsu@gmail.com>
2015-06-29 17:21:20 +0900

    update rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
@@ -25,4 +27,5 @@ RSpec.configure do |config|
   end
 
   config.include FactoryGirl::Syntax::Methods
+  config.include FeatureHelper, type: :feature
 end
