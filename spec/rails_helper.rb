14bf8bf10688b3087842c7a3953c754b48751c1d
tetsu <soutetsu@gmail.com>
2015-07-04 15:55:20 +0900

    update rspec configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,3 +33,6 @@ RSpec.configure do |config|
 end
 
 Capybara.javascript_driver = :poltergeist
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, inspector: true)
+end
